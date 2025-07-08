8eaa78f147a45068166f2b263a97974cbef96520
Iman Iqor <imiqor@c3r9p9.1337.ma>
2025-07-08 01:19:04 +0100

    draw_map_and_player not completed

--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: imiqor <imiqor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 00:50:48 by imiqor            #+#    #+#             */
-/*   Updated: 2025/07/07 23:17:46 by imiqor           ###   ########.fr       */
+/*   Updated: 2025/07/07 23:55:25 by imiqor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@
 # include "./minilibx-linux/mlx.h"
 # include <errno.h>
 # include <fcntl.h>
+# include <math.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include<math.h>
 
 typedef struct s_state
 {
-	int		i;
-	int		cw;
-	int		start;
-	int		end;
-	int		k;
-	char	**two_d;
-}			t_state;
+	int			i;
+	int			cw;
+	int			start;
+	int			end;
+	int			k;
+	char		**two_d;
+}				t_state;
 
 typedef struct s_gc
 {
@@ -38,90 +38,93 @@ typedef struct s_gc
 	struct s_gc	*next;
 }				t_gc;
 
-
 typedef struct s_map
 {
-	int		fd;
-	int		line_count;
-	int		grid_lines_count;
-	char	**map_two_d;
-	char	**map_grid;
-	int		player_x;
-	int		player_y;
-	char	player_dir;
-	int		map_width;
-	int		map_height;
-	char	*lines;
-	char	*no;
-	char	*so;
-	char	*we;
-	char	*ea;
+	int			fd;
+	int			line_count;
+	int			grid_lines_count;
+	char		**map_two_d;
+	char		**map_grid;
+	int			player_x;
+	int			player_y;
+	char		player_dir;
+	int			map_width;
+	int			map_height;
+	char		*lines;
+	char		*no;
+	char		*so;
+	char		*we;
+	char		*ea;
 	int has_no, has_so, has_we, has_ea;
 	int has_floor, has_ceiling;
 	int floor_r, floor_g, floor_b;
 	int ceiling_r, ceiling_g, ceiling_b;
-}			t_map;
+}				t_map;
 
 typedef struct s_game
 {
-	void	*mlx;       // MLX connection
-	void	*win;       // Window
-	int		win_width;
-	int		win_height;
-	t_map	map;        // Map data you already parsed
-	double	player_x;
-	double	player_y;
-	double	dir_x;       // Player direction vector (for raycasting)
-	double	dir_y;
-	double	plane_x;     // 2D camera plane
-	double	plane_y;
-}	t_game;
+	void *mlx; // MLX connection
+	void *win; // Window
+	int			win_width;
+	int			win_height;
+	t_map map; // Map data you already parsed
+	double		player_x;
+	double		player_y;
+	double dir_x; // Player direction vector (for raycasting)
+	double		dir_y;
+	double plane_x; // 2D camera plane
+	double		plane_y;
+	void		*wall_no;
+	void		*wall_so;
+	void		*wall_we;
+	void		*wall_ea;
+}				t_game;
 /******  SPLIT   ****** */
-char		*ftt_free(char **arr);
-char		*ft_strncopy(char *str, int start, int end);
-int			count_word(char *args, char sep);
-void		init_state(t_state *state, char *args, char sep);
-char		**split_lines(char *args, char sep);
+char			*ftt_free(char **arr);
+char			*ft_strncopy(char *str, int start, int end);
+int				count_word(char *args, char sep);
+void			init_state(t_state *state, char *args, char sep);
+char			**split_lines(char *args, char sep);
 /******  TEXTURES   ****** */
-void		check_path(char *path);
-char		*trim_spaces_end(char *str);
-void		assign_texture(t_map *map, char *line, char **dest, char *id);
-void		parse_textures_and_colors(char **lines, int *i, t_map *map);
+void			check_path(char *path);
+char			*trim_spaces_end(char *str);
+void			assign_texture(t_map *map, char *line, char **dest, char *id);
+void			parse_textures_and_colors(char **lines, int *i, t_map *map);
 /******  COLORS   ****** */
-int			parse_rgb(char *line, int *r, int *g, int *b);
-void		set_floor_color(t_map *map, char *line);
-void		set_ceiling_color(t_map *map, char *line);
+int				parse_rgb(char *line, int *r, int *g, int *b);
+void			set_floor_color(t_map *map, char *line);
+void			set_ceiling_color(t_map *map, char *line);
 /******  ARGV1  ****** */
-void parssing(t_map* map,int argc,char** argv);
-void	check_map_content(char **content, t_map *map);
-int			check_if_file_exist(char *argv);
-void		check_extention(char *argv);
-void		check_argc(int argc);
+void			parssing(t_map *map, int argc, char **argv);
+void			check_map_content(char **content, t_map *map);
+int				check_if_file_exist(char *argv);
+void			check_extention(char *argv);
+void			check_argc(int argc);
 /******  MAP_CONTENT  ****** */
-int			nbr_of_lines(char *filename);
-void		get_cub_content(char *argv, t_map *map);
+int				nbr_of_lines(char *filename);
+void			get_cub_content(char *argv, t_map *map);
 /******  PARSE_MAP  ****** */
-int			count_map_lines(char **content, int start);
-void		trim_newline(char *str);
-int			contains_zero(char *line);
-void		store_map_lines(char **content, int start, t_map *map);
-int			is_valid_map_char(char c);
-void		validate_map_chars(t_map *map);
-void		validate_walls(t_map *map);
-int			ft_check_is_player(char c);
-void		parse_map(t_map *map, char **content, int start);
+int				count_map_lines(char **content, int start);
+void			trim_newline(char *str);
+int				contains_zero(char *line);
+void			store_map_lines(char **content, int start, t_map *map);
+int				is_valid_map_char(char c);
+void			validate_map_chars(t_map *map);
+void			validate_walls(t_map *map);
+int				ft_check_is_player(char c);
+void			parse_map(t_map *map, char **content, int start);
 /******  UTILS  ****** */
-char		*ft_ssstrjoin(char *save, char *buff);
-int			ft_strcmp(char *s1, char *s2);
-int			ft_strcmp(char *s1, char *s2);
-void		error_exit(char *msg);
-int			is_blank(char *s);
-void		check_no_blank_lines_inside_map(char **content, int start);
-char	*ftt_strdup(const char *s1);
+char			*ft_ssstrjoin(char *save, char *buff);
+int				ft_strcmp(char *s1, char *s2);
+int				ft_strcmp(char *s1, char *s2);
+void			error_exit(char *msg);
+int				is_blank(char *s);
+void			check_no_blank_lines_inside_map(char **content, int start);
+char			*ftt_strdup(const char *s1);
 /******  GARBAGE_COLLECTOR ****** */
-void	add(t_gc **gc, t_gc *new);
-t_gc	*create(void *ptr);
-void	fr_ee(t_gc *gc);
-void	*ft_gc(size_t n, char flag);
+void			add(t_gc **gc, t_gc *new);
+t_gc			*create(void *ptr);
+void			fr_ee(t_gc *gc);
+void			*ft_gc(size_t n, char flag);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: imiqor <imiqor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 00:51:17 by imiqor            #+#    #+#             */
-/*   Updated: 2025/07/07 23:16:04 by imiqor           ###   ########.fr       */
+/*   Updated: 2025/07/08 01:07:53 by imiqor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "header.h"
 
 
-#include "header.h"
 
 void	init_game(t_game *game, t_map *map)
 {
@@ -25,7 +24,10 @@ void	init_game(t_game *game, t_map *map)
 		error_exit("MLX failed to init");
 	game->win = mlx_new_window(game->mlx, game->win_width, game->win_height, "cub3D");
 	if (!game->win)
+	{
+		mlx_destroy_display(game->mlx);
 		error_exit("Failed to open window");
+	}
 
 	game->map = *map;
 
@@ -40,19 +42,73 @@ void	init_game(t_game *game, t_map *map)
 	if (map->player_dir == 'W') { game->dir_x = -1; game->dir_y = 0; game->plane_x = 0; game->plane_y = -0.66; }
 }
 
+void image_failed_to_load(t_game* game)
+{
+	if(!game->wall_no || !game->wall_so || !game->wall_ea || !game->wall_we)
+	{
+		if(game->wall_no)
+			mlx_destroy_image(game->mlx,game->wall_no);
+		if(game->wall_so)
+			mlx_destroy_image(game->mlx,game->wall_so);
+		if(game->wall_ea)
+			mlx_destroy_image(game->mlx,game->wall_ea);
+		mlx_destroy_window(game->mlx,game->win);
+		mlx_destroy_display(game->mlx);
+		error_exit("error: could not load one or more images");
+	}
+}
 
+void load_images(t_game *game,t_map *map)
+{
+	(void) map;
+	int h;
+	int w;
+	game->wall_no = mlx_xpm_file_to_image(game->mlx,"./textures/north.xpm",&w,&h);
+	game->wall_so = mlx_xpm_file_to_image(game->mlx,"./textures/south.xpm",&w,&h);
+	game->wall_we = mlx_xpm_file_to_image(game->mlx,"./textures/west.xpm",&w,&h);
+	game->wall_no = mlx_xpm_file_to_image(game->mlx,"./textures/east.xpm",&w,&h);
+	image_failed_to_load(game);
+}
+
+void draw_map_and_player(t_game* game,t_map* map)
+{
+	int i;
+	int j ;
+	i = 0;
+
+	while(map->map_grid[i])
+	{
+		j = 0;
+		while(map->map_grid[i][j])
+		{
+			if(map->map_grid[i][j] == '1')
+				draw_tile();
+			else
+				draw_time();
+			j++;
+		}
+		i++;
+	}
+	draw_player(game,map);
+}
 
 int	main(int argc, char **argv)
 {
 	t_map	map;
-	// t_game game;
+	t_game game;
 	parssing(&map,argc,argv);
 	
 
 
-	/****Start the game*** */
-	// init_game(&game,&map);
-	// mlx_loop(game.mlx);
+	
+	init_game(&game,&map);
+	draw_map_and_player(&game,&map);
+	load_images(&game,&map);
+	mlx_loop(game.mlx);
+
+
+
+	
 	ft_gc(0,'f');
 	return (0);
 }
