11985525aa043f4b13f34c0a2d7c9bbaa1504525
Iman Iqor <imiqor@e2r13p10.1337.ma>
2025-11-01 18:46:48 +0100

    last_commit

--- parssing/textures2.c
+++ parssing/textures2.c
@@ -6,7 +6,7 @@
 /*   By: imiqor <imiqor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 18:40:40 by imiqor            #+#    #+#             */
-/*   Updated: 2025/10/31 19:08:08 by imiqor           ###   ########.fr       */
+/*   Updated: 2025/11/01 18:42:54 by imiqor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ char	*trim_spaces_end(char *str)
 	if (!str)
 		return (NULL);
 	len = ft_strlen(str);
-	while (len > 0 && (str[len - 1] == ' ' || str[len - 1] == '\n' || str[len
-			- 1] == '\t'))
+	while (len > 0 && (str[len - 1] == ' ' || str[len - 1] == '\n'))
 		len--;
 	trimmed = ft_gc(len + 1, 'm');
 	if (!trimmed)
